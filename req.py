9b8d8e5c055d5eeb531bb01e051238b32c80433f
Kamleshprajput <145245907+Kamleshprajput@users.noreply.github.com>
2026-03-28 11:38:04 +0530

    changes

--- req.py
+++ req.py
@@ -1,10 +1,1 @@
 import requests
-
-url = "https://api.elevenlabs.io/v1/voices"
-
-headers = {
-    "xi-api-key": "sk_866d22f516d4d2587824743b4c438aa7649f139662b185d2"
-}
-
-resp = requests.get(url, headers=headers)
-print(resp.json())
